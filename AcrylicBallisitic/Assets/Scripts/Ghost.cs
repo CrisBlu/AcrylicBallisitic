e17e9241627fc7525d689b51c25ab15a491340c8
Stanley Freihofer <stanleyfreihofer@gmail.com>
2026-02-20 17:51:23 -0500

    Ghost

--- AcrylicBallisitic/Assets/Scripts/Ghost.cs
+++ AcrylicBallisitic/Assets/Scripts/Ghost.cs
@@ -12,15 +12,16 @@ public class Ghost : MonoBehaviour
     public float minSpawnDist = 3f;
     public float maxSpawnDist = 6f;
 
+    public GameObject telegraphVisual; // A flat disk or sphere child object
+
     private Transform playerTransform;
 
     void Start()
     {
         GameObject pObj = GameObject.FindGameObjectWithTag("Player");
-       // if (pObj) player = pObj.transform;
-
-        // Hide the telegraph visual initially
-      //  if (telegraphVisual) telegraphVisual.SetActive(false);
+        if (pObj) playerTransform = pObj.transform;
+        if (telegraphVisual) telegraphVisual.SetActive(false);
+        StartCoroutine(AttackLoop());
     }
 
     IEnumerator AttackLoop()
@@ -34,6 +35,34 @@ public class Ghost : MonoBehaviour
                 // 1. Calculate Random Position around player
                 Vector2 randomCircle = Random.insideUnitCircle.normalized * Random.Range(minSpawnDist, maxSpawnDist);
                 Vector3 targetPos = playerTransform.position + new Vector3(randomCircle.x, 0, randomCircle.y);
+
+                telegraphVisual.transform.position = targetPos - Vector3.up * 0.5f; // Slight floor offset
+                telegraphVisual.SetActive(true);
+                telegraphVisual.transform.localScale = Vector3.one * 0.1f;
+
+                //float up
+                float elapsed = 0;
+                while (elapsed < fuseTime)
+                {
+                    elapsed += Time.deltaTime;
+                    telegraphVisual.transform.localScale = Vector3.Lerp(Vector3.one * 0.1f, Vector3.one * blastRadius, elapsed / fuseTime);
+                    telegraphVisual.transform.position += Vector3.up * (Time.deltaTime / fuseTime);
+                    yield return null;
+                }
+                Explode(targetPos);
+            }
+        }
+
+        void Explode(Vector3 center)
+        {
+            Debug.Log("Boom");
+            Collider[] hits = Physics.OverlapSphere(center, blastRadius);
+            foreach (var hit in hits)
+            {
+                if (hit.CompareTag("Player"))
+                {
+                    Debug.Log("Player Caught in Blast!");
+                }
             }
         }
     }
