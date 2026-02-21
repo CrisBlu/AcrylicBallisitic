4d861bb1d88021c72823cfdb4423df1a1fdb391f
BrendanStEsprit <weegeegotme@gmail.com>
2026-02-21 14:53:07 -0500

    Added Chair model, added Animation Controller assets

---
+++ AcrylicBallisitic/Assets/Scripts/StanAnimCtrl.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class StanAnimCtrl : MonoBehaviour
+{
+
+    private Animator AnimationController;
+    private Rigidbody Rigid;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        AnimationController = GetComponent<Animator>();
+
+       Rigid =  GetComponentInParent<Rigidbody>();
+
+        
+
+        if (AnimationController == null)
+        {
+            Debug.LogError("Animator component not found on this GameObject!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AnimationController.SetFloat("PlayerVelocity",Rigid.velocity.x + Rigid.velocity.z);
+
+        print("Stans velocity is" + Rigid.velocity.x + Rigid.velocity.z);
+    }
+}
