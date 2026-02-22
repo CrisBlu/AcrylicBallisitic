cb9dbc9868d80e638a350bd5c7ab21368efa5f1c
CrisBlu <55038118+CrisBlu@users.noreply.github.com>
2026-02-21 21:46:59 -0500

    Main menu done

--- AcrylicBallisitic/Assets/Scripts/MainMenuCamera.cs
+++ AcrylicBallisitic/Assets/Scripts/MainMenuCamera.cs
@@ -1,14 +1,34 @@
+using System;
+using System.Threading.Tasks;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class MainMenuCamera : MonoBehaviour
 {
     [SerializeField] float speed;
     [SerializeField] Transform house;
+    [SerializeField] Image panel;
     //float angle = 0;
     // Update is called once per frame
     void Update()
     {
-        transform.RotateAround(house.position, Vector3.up, 20 * Time.captureDeltaTime);
-        //angle += speed;
+        transform.LookAt(house);
+        transform.Translate(Vector3.right * Time.deltaTime * speed);
+    }
+
+    public async void GoIn()
+    {
+        speed = 0;
+        Color newColor = new Color(panel.color.r, panel.color.g, panel.color.b, 0);
+        Debug.Log("Going in");
+        while(Vector3.Distance(transform.position, house.position) > .1f)
+        {
+            newColor.a += .05f;
+            panel.color = newColor;
+            transform.Translate(Vector3.forward * Time.deltaTime * 200);
+            await Task.Yield();
+        }
+
+        Debug.Log("In");
     }
 }
+
--- AcrylicBallisitic/Assets/Scripts/MenuButton.cs
+++ AcrylicBallisitic/Assets/Scripts/MenuButton.cs
@@ -1,3 +1,5 @@
+using System.Threading.Tasks;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,6 +7,20 @@ public class MenuButton : MonoBehaviour
 {
     public void SwitchScenes(int scene)
     {
+        SceneManager.LoadScene(scene);
+    }
+
+    public async void DelaySwitchScenes(int scene)
+    {
+        float timer = 0;
+        float duration = 2f;
+
+        while (timer < duration)
+        {
+            timer += Time.deltaTime;
+            await Task.Yield();
+        }
+
         SceneManager.LoadScene(scene);
     }
 }
